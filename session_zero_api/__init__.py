685953160244b8106cf19aae8664bc91c750078f
kemeen <kevin.m.engel@gmail.com>
2024-07-26 17:03:39 +0000

    set logging level to WARNING

--- session_zero_api/__init__.py
+++ session_zero_api/__init__.py
@@ -5,11 +5,11 @@ import pathlib
 def get_logger(name: str) -> logging.Logger:
     # setup logger
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.WARNING)
     
     # add stream handler
     ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    ch.setLevel(logging.WARNING)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
@@ -19,7 +19,7 @@ def get_logger(name: str) -> logging.Logger:
     log_path.parent.mkdir(parents=True, exist_ok=True)
     log_path.touch(exist_ok=True)
     fh = logging.FileHandler(filename=log_path, mode='a', encoding='utf-8')
-    fh.setLevel(logging.DEBUG)
+    fh.setLevel(logging.WARNING)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     return logger
